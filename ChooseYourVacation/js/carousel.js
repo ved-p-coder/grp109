14968d4fd94d9039db366068f9bfcaf0cca84ca5
Lyvdev109 <olyvia.lindner@bellevuecollege.edu>
2025-06-17 23:16:20 -0700

    Update carousel.js
    carousel js

--- ChooseYourVacation/js/carousel.js
+++ ChooseYourVacation/js/carousel.js
@@ -1,1 +1,115 @@
+var myImages = [
+  "images/city/paris.jpg",
+  "images/city/newyork.jpg",
+  "images/city/london.jpg",
+  "images/city/istanbul.jpg",
+  "images/city/hongkong.jpg",
+  "images/city/rome.jpg",
+  "images/city/dubai.jpg",
+  "images/city/venice.jpg",
+  "images/city/seoul.jpg",
+  "images/city/cairo.jpg"
+  ];
 
+var captionImage = [
+  "Enjoy iconic landmarks in Paris",
+  "Explore New York City",
+  "Walk through Londons rich history",
+  "See the views of Instanbul and its unique position staddling two continents",
+  "Find yourself immersed in Hong Kongs iconic skyline of skyscrapers",
+  "See Rome's unique history, art, and architecture",
+  "Travel to Dubai to experience modern architecture and luxery shopping",
+  "Float through the unique network of canals in Venice",
+  "Experience rich cultural heritage in Seoul",
+  "Tour the great Ciaro and fall in love with its ancient landmarks"
+  ];
+
+var index = 0;
+var timerInterval; // to store ID for clearing
+var currentCountdown = 4; // countdown value
+const SLIDESHOW_DURATION = 4;
+
+// get elements
+var slideshowImage = document.getElementById("slideshow");
+var captionParagraph = document.getElementById("caption");
+var timerDisplay = document.getElementById("timerDisplay");
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+var autoCheckbox = document.getElementById("auto");
+
+// function to update the displayed image and caption
+function updateImage (){
+  slideshowImage.src = myImages[index];
+  slideshowImage.alt = captionImage[index];
+  captionParagraph.textContent = captionImages[index];
+  resetTimer(); // resets the timer whenever the image updates
+}
+
+//function to advance to next image
+function next() {
+  if (myImages.length === index + 1) {
+    index = 0; // loop back to the first image if at the end
+  } else {
+    index +++;
+  }
+  updateImage(); // updates image and resets timer
+}
+
+// function to go back to the previous image
+function back() {
+  if (index === 0) {
+    index = myImages.length - 1; // loops to the last image
+  } else {
+    index--;
+  }
+  updateImage(); //updates image and resets timer
+}
+
+//function to update countdown and trigger image change
+function updateCountdown () {
+  currentCountdown--;
+  timerDisplay.textContent = currentCountdown;
+
+  if (currentCountdown <= 0) {
+    clearInterval(timerInterval); //stop current countdown
+    next(); //move to next image
+  }
+}
+
+// function to start or restart timer
+function startTimer () {
+  clearInterval(timerInterval)
+  currentCountdown = SLIDESHOW_DURATION;
+  timerDisplay.textContent = currentCountdown;
+  timerInterval = setInterval(updateCountdown, 1000);
+}
+
+//function to reset automatic slideshow timer
+function resetTimer () {
+  clearInterval(timerInterval);
+  if (autoCheckbox.checked) {
+    startTimer();
+  } else {
+    currentCountdown = SLIDESHOW_DURATION;
+    timerDisplay.textContent = currentCountdown;
+  }
+}
+
+// function for "run automatically"
+function autoSlide () {
+  if (autoCheckbox.checked) {
+    resetTimer();
+  } else {
+    clearInterval(timerInterval);
+    currentCountdown = SLIDESHOW_DURATION;
+    timerDisplay.textContent = currentCountdown;
+  }
+}
+
+// -- event listeners --
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
+autoCheckbox.addEventListener("change", autoSlide, false);
+
+updateImage();
+autoslide();
