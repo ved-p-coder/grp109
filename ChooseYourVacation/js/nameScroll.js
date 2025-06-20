b841c0733cac4ffe2c8ccc55d44af4b3daa2ff36
Ben <124755801+roar-k@users.noreply.github.com>
2025-06-20 00:45:16 -0700

    Create nameScroll.js

---
+++ ChooseYourVacation/js/nameScroll.js
@@ -0,0 +1,9 @@
+var names = ["Ben", "Lyv", "Ved", "Will"];
+var index = 0;
+
+function scrollNames() {
+    document.getElementById("name-scroll").textContent = names[index];
+    index = (index + 1) % names.length;
+  }
+
+  setInterval(scrollNames, 1500); // Change every 1.5 seconds
