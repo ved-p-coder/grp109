ec362dc7d94ffe65bb97275e392c6b389cee22f3
Ben <124755801+roar-k@users.noreply.github.com>
2025-06-22 00:12:30 -0700

    Update displayList.js

--- ChooseYourVacation/js/displayList.js
+++ ChooseYourVacation/js/displayList.js
@@ -16,17 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Displays all the form information
   var display = "";
-  html += "<p><strong>First Name:</strong> " + data.firstName + "</p>";
-  html += "<p><strong>Last Name:</strong> " + data.lastName + "</p>";
-  html += "<p><strong>Email:</strong> " + data.email + "</p>";
-  html += "<p><strong>Phone:</strong> " + data.phone + "</p>";
-  html += "<p><strong>Username:</strong> " + data.username + "</p>";
-  html += "<p><strong>Password:</strong> " + data.password + "</p>";
-  html += "<p><strong>Address:</strong> " + data.address + "</p>";
-  html += "<p><strong>City:</strong> " + data.city + "</p>";
-  html += "<p><strong>State:</strong> " + data.state + "</p>";
-  html += "<p><strong>Country:</strong> " + data.country + "</p>";
-  html += "<p><strong>Zip Code:</strong> " + data.zipCode + "</p>";
+  display += "<p><strong>First Name:</strong> " + data.firstName + "</p>";
+  display += "<p><strong>Last Name:</strong> " + data.lastName + "</p>";
+  display += "<p><strong>Email:</strong> " + data.email + "</p>";
+  display += "<p><strong>Phone:</strong> " + data.phone + "</p>";
+  display += "<p><strong>Username:</strong> " + data.username + "</p>";
+  display += "<p><strong>Password:</strong> " + data.password + "</p>";
+  display += "<p><strong>Address:</strong> " + data.address + "</p>";
+  display += "<p><strong>City:</strong> " + data.city + "</p>";
+  display += "<p><strong>State:</strong> " + data.state + "</p>";
+  display += "<p><strong>Country:</strong> " + data.country + "</p>";
+  display += "<p><strong>Zip Code:</strong> " + data.zipCode + "</p>";
   
-  output.innerHTML = display;
+  output.innerdisplay = display;
 });
